a54c5fd7d7d01492906b2917eb201ca7ef68aa8c
B <bryanandersen03@gmail.com>
2023-10-27 20:05:38 +0800

    Final Result

--- MyDay/VideoView.swift
+++ MyDay/VideoView.swift
@@ -2,17 +2,42 @@
 //  VideoView.swift
 //  MyDay
 //
-//  Created by Bryan Andersen on 2023/10/27.
+//  Created by Bryan Andersen on 2023/10/23.
 //
 
 import SwiftUI
+import WebKit
+
+struct YouTubeView: UIViewRepresentable {
+    let videoId: String
+    func makeUIView(context: Context) ->  WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let demoURL = URL(string: "https://www.youtube.com/embed/\(videoId)") else { return }
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: demoURL))
+    }
+}
 
 struct VideoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("國立臺灣海洋大學\n70週年校慶")
+                .multilineTextAlignment(.center)
+                .font(.custom("SourceHanSerifTC-Bold", fixedSize: 35))
+            YouTubeView(videoId: "20EJUCYmdyQ")
+                .scaledToFit()
+                .padding()
+            Spacer()
+        }
     }
 }
 
 #Preview {
     VideoView()
 }
+
+
+
+
