d520a306060579d5e1d29cfdcb9a130920642bf7
B <bryanandersen03@gmail.com>
2023-10-27 18:30:01 +0800

    Initial Commit

---
+++ MyDay/MyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDayApp.swift
+//  MyDay
+//
+//  Created by Bryan Andersen on 2023/10/27.
+//
+
+import SwiftUI
+
+@main
+struct MyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
